e16387f0350b0707bb41f8d1453c83c5551fa25c
Vishal Madhvani <vishal@vishalmadhvani.com>
2019-09-27 19:54:17 +0100

    Redesign without durable functions

--- CopyBlobMetadata/CopyBlobMetadataFunctions.cs
+++ CopyBlobMetadata/CopyBlobMetadataFunctions.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -14,6 +15,64 @@ namespace blobmetadataupdate
 {
     public static class CopyBlobMetadataFunctions
     {
+        [FunctionName("Start")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            CloudQueue queue = GetQueue("getblobs");
+            await queue.AddMessageAsync(new CloudQueueMessage("START"));
+
+            return new OkResult();
+        }
+
+        [FunctionName("GetBlobs")]
+        public static async Task GetBlobs(
+            [QueueTrigger("getblobs", Connection = "CopyBlobMetadataQueueConnectionString")]string continuationToken,
+            ILogger log)
+        {
+            if (continuationToken == "START")
+                continuationToken = null;
+
+            CloudBlobContainer cloudBlobContainer = GetSourceBlobContainer();
+            var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, 5000, new BlobContinuationToken() { NextMarker = continuationToken }, null, null);
+
+            continuationToken = results.ContinuationToken?.NextMarker;
+            if (continuationToken != null)
+            {
+                CloudQueue getBlobsQueue = GetQueue("getblobs");
+                await getBlobsQueue.AddMessageAsync(new CloudQueueMessage(continuationToken));
+            }
+
+            CloudQueue copyMetadataQueue = GetQueue("copymetadata");
+            List<string> blobNames = results.Results.OfType<CloudBlockBlob>().Select(blob => blob.Name).ToList();
+            foreach (string blobName in blobNames)
+            {
+                await copyMetadataQueue.AddMessageAsync(new CloudQueueMessage(blobName));
+            }
+        }
+
+        [FunctionName("CopyMetadata")]
+        public static async Task UpdateMetadata(
+            [QueueTrigger("copymetadata", Connection = "CopyBlobMetadataQueueConnectionString")]string blobName,
+            ILogger log)
+        {
+            var sourceBlobContainer = GetSourceBlobContainer();
+            var sourceBlob = sourceBlobContainer.GetBlockBlobReference(blobName);
+            await sourceBlob.FetchAttributesAsync();
+
+            var destinationBlobContainer = GetDestinationBlobContainer();
+            var destinationBlob = destinationBlobContainer.GetBlockBlobReference(blobName);
+            //await destinationBlob.FetchAttributesAsync(); // Do we need to fetch given we're overwriting?
+
+            destinationBlob.Metadata.Clear();
+            foreach (var metadata in sourceBlob.Metadata)
+            {
+                destinationBlob.Metadata.Add(metadata.Key, metadata.Value);
+            }
+            await destinationBlob.SetMetadataAsync();
+        }
+
         private static CloudBlobContainer GetSourceBlobContainer()
         {
             return GetBlobContainer(Environment.GetEnvironmentVariable("SourceConnectionString"), Environment.GetEnvironmentVariable("SourceContainerName"));
@@ -32,131 +91,13 @@ namespace blobmetadataupdate
             return cloudBlobContainer;
         }
 
-        private static CloudQueue GetQueue()
+        private static CloudQueue GetQueue(string queueName)
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("CopyBlobMetadataQueueConnectionString"));
             CloudQueueClient client = storageAccount.CreateCloudQueueClient();
-            CloudQueue queue = client.GetQueueReference("copyblobmetadata");
+            CloudQueue queue = client.GetQueueReference(queueName);
+            queue.CreateIfNotExistsAsync();
             return queue;
-        }
-
-        public class CopyBlobMetadataInput
-        {
-            public string Source { get; set; }
-            public string Destination { get; set; }
-        }
-
-        [FunctionName("CopyBlobMetadata")]
-        public static async Task CopyBlobMetadata(
-            [OrchestrationTrigger] DurableOrchestrationContext context,
-            ILogger log)
-        {
-            //var input = context.GetInput<CopyBlobMetadataInput>();
-            List<Task> queueIndividualBlobNamesFromSegmentTasks = new List<Task>();
-            BlobContinuationToken blobContinuationToken = null;
-            while (true)
-            {
-                // Verify: Is null a value activity input and hence is the response correctly cached?
-                // TODO: Use string continuationToken
-                var response = await context.CallActivityAsync<GetBlobNamesInSegmentsOutput>("GetBlobNamesInSegments", blobContinuationToken);
-
-                Task task = context.CallActivityAsync("QueueIndividualBlobNamesFromSegment", response.BlobNames);
-                queueIndividualBlobNamesFromSegmentTasks.Add(task);
-
-                blobContinuationToken = response.BlobContinuationToken;
-                if (blobContinuationToken is null) break;
-            }
-
-            await Task.WhenAll(queueIndividualBlobNamesFromSegmentTasks);
-
-            // TODO: wait until UpdateMetadata functions have completed so Status can be tracked
-            //      wait until queue is empty and stable? 
-        }
-
-        public class GetBlobNamesInSegmentsOutput
-        {
-            public BlobContinuationToken BlobContinuationToken { get; set; }
-            public List<string> BlobNames { get; set; }
-        }
-
-        // TODO: InputDto
-        [FunctionName("GetBlobNamesInSegments")]
-        public static async Task<GetBlobNamesInSegmentsOutput> GetBlobNamesInSegments(
-            [ActivityTrigger] BlobContinuationToken blobContinuationToken,
-            ILogger log)
-        {
-            CloudBlobContainer cloudBlobContainer = GetSourceBlobContainer();
-
-            // TODO: Review docs on available overloads for this method.
-            //      What does useFlatBlobListing do?
-            //      find a good batch size
-            var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
-
-            // Will OfType actually filter to block blobs?
-            List<string> blobNames = results.Results.OfType<CloudBlockBlob>().Select(blob => blob.Name).ToList();
-
-            var output = new GetBlobNamesInSegmentsOutput()
-            {
-                BlobContinuationToken = results.ContinuationToken,
-                BlobNames = blobNames
-            };
-            return output;
-        }
-
-        // Can this be a SubOrchestrator?
-        [FunctionName("QueueIndividualBlobNamesFromSegment")]
-        public static async Task QueueIndividualBlobNamesFromSegment(
-            [ActivityTrigger] List<string> blobNames,
-            ILogger log)
-        {
-            CloudQueue queue = GetQueue();
-
-            foreach (string blobName in blobNames)
-            {
-                await queue.AddMessageAsync(new CloudQueueMessage(blobName));
-            }
-        }
-
-        [FunctionName("Start")]
-        public static async Task<HttpResponseMessage> Start(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
-            [OrchestrationClient]DurableOrchestrationClient starter,
-            ILogger log)
-        {
-            //var options = JsonConvert.DeserializeObject<JObject>(await req.Content.ReadAsStringAsync());
-            //var copyBlobMetadataInput = new CopyBlobMetadataInput()
-            //{
-            //    Source = options.Value<string>("source"),
-            //    Destination = options.Value<string>("destination")
-            //};
-
-            string instanceId = await starter.StartNewAsync("CopyBlobMetadata", null);
-
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
-
-            return starter.CreateCheckStatusResponse(req, instanceId);
-        }
-
-        // Can this be an Activity?
-        [FunctionName("CopyMetadata")]
-        public static async Task UpdateMetadata(
-            [QueueTrigger("copyblobmetadata", Connection = "CopyBlobMetadataQueueConnectionString")]string blobName,
-            ILogger log)
-        {
-            var sourceBlobContainer = GetSourceBlobContainer();
-            var sourceBlob = sourceBlobContainer.GetBlockBlobReference(blobName);
-            await sourceBlob.FetchAttributesAsync();
-
-            var destinationBlobContainer = GetDestinationBlobContainer();
-            var destinationBlob = destinationBlobContainer.GetBlockBlobReference(blobName);
-            //await destinationBlob.FetchAttributesAsync(); // Do we need to fetch given we're overwriting?
-
-            destinationBlob.Metadata.Clear();
-            foreach (var metadata in sourceBlob.Metadata)
-            {
-                destinationBlob.Metadata.Add(metadata.Key, metadata.Value);
-            }
-            await destinationBlob.SetMetadataAsync();
         }
     }
 }
