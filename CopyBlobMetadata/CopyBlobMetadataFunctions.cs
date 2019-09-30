4392d756e5e52652be33ea5d9f3b7c7e654e1acc
Vishal Madhvani <vishal@vishalmadhvani.com>
2019-09-30 12:34:46 +0100

    Distribute queuing of blobs names. Use HttpTriggers and log executions to a table.

--- CopyBlobMetadata/CopyBlobMetadataFunctions.cs
+++ CopyBlobMetadata/CopyBlobMetadataFunctions.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,60 +13,127 @@ using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace blobmetadataupdate
 {
     public static class CopyBlobMetadataFunctions
     {
+        private static HttpClient httpClient = new HttpClient();
+        private static string functionAppUrl = Environment.GetEnvironmentVariable("FunctionAppUrl");
+        private static string functionStorageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
         [FunctionName("Start")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public static async Task<IActionResult> Start(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            CloudQueue queue = GetQueue("getblobs");
-            await queue.AddMessageAsync(new CloudQueueMessage("START"));
+            var copyMetadataInfo = await CopyMetadataInfo.Deserialize(req.Body);
+            copyMetadataInfo.ExecutionId = Guid.NewGuid().ToString();
 
+            if (string.IsNullOrWhiteSpace(copyMetadataInfo.SourceConnectionString)) throw new ArgumentNullException("SourceConnectionString");
+            if (string.IsNullOrWhiteSpace(copyMetadataInfo.SourceContainerName)) throw new ArgumentNullException("SourceContainerName");
+            if (string.IsNullOrWhiteSpace(copyMetadataInfo.DestinationConnectionString)) throw new ArgumentNullException("DestinationConnectionString");
+            if (string.IsNullOrWhiteSpace(copyMetadataInfo.DestinationContainerName)) throw new ArgumentNullException("DestinationContainerName");
+
+            await EnsureInfrastructure();
+
+            _ = httpClient.PostAsync($"{functionAppUrl}GetBlobs", new StringContent(copyMetadataInfo.ToString()));
             return new OkResult();
         }
 
         [FunctionName("GetBlobs")]
         public static async Task GetBlobs(
-            [QueueTrigger("getblobs", Connection = "CopyBlobMetadataQueueConnectionString")]string continuationToken,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            if (continuationToken == "START")
-                continuationToken = null;
+            var stopwatch = new Stopwatch();
+            req.GetQueryParameterDictionary().TryGetValue("ContinuationToken", out var continuationToken);
+            var copyMetadataInfo = await CopyMetadataInfo.Deserialize(req.Body);
+            var blobContainer = GetBlobContainer(copyMetadataInfo.SourceConnectionString, copyMetadataInfo.SourceContainerName);
+            var startedAt = DateTime.UtcNow;
 
-            CloudBlobContainer cloudBlobContainer = GetSourceBlobContainer();
-            var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, 5000, new BlobContinuationToken() { NextMarker = continuationToken }, null, null);
+            stopwatch.Start();
+            var results = await blobContainer.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, 5000, new BlobContinuationToken() { NextMarker = continuationToken }, null, null);
+            stopwatch.Stop();
 
             continuationToken = results.ContinuationToken?.NextMarker;
-            if (continuationToken != null)
+            if (!string.IsNullOrWhiteSpace(continuationToken))
             {
-                CloudQueue getBlobsQueue = GetQueue("getblobs");
-                await getBlobsQueue.AddMessageAsync(new CloudQueueMessage(continuationToken));
+                _ = httpClient.PostAsync($"{functionAppUrl}GetBlobs?ContinuationToken={continuationToken}", new StringContent(copyMetadataInfo.ToString()));
             }
+            await LogListBlobs(copyMetadataInfo.ExecutionId, startedAt, stopwatch.ElapsedMilliseconds, results.Results.Count());
 
-            CloudQueue copyMetadataQueue = GetQueue("copymetadata");
-            List<string> blobNames = results.Results.OfType<CloudBlockBlob>().Select(blob => blob.Name).ToList();
-            foreach (string blobName in blobNames)
+            List<CloudBlockBlob> blobs = results.Results.OfType<CloudBlockBlob>().ToList();
+            var tracker = 0;
+            while (tracker < blobs.Count)
             {
-                await copyMetadataQueue.AddMessageAsync(new CloudQueueMessage(blobName));
+                var take = blobs.Count - tracker > 500 ? 500 : blobs.Count - tracker;
+                var pagedResults = blobs.Skip(tracker).Take(take);
+                var jsonContent = JsonConvert.SerializeObject(pagedResults.Select(blob => new CopyMetadataInfo()
+                {
+                    ExecutionId = copyMetadataInfo.ExecutionId,
+                    BlobName = blob.Name,
+                    //Metadata = blob.Metadata.ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                    SourceConnectionString = copyMetadataInfo.SourceConnectionString,
+                    SourceContainerName = copyMetadataInfo.SourceContainerName,
+                    DestinationConnectionString = copyMetadataInfo.DestinationConnectionString,
+                    DestinationContainerName = copyMetadataInfo.DestinationContainerName,
+                }));
+                var httpContent = new StringContent(jsonContent);
+                _ = httpClient.PostAsync($"{functionAppUrl}ProcessSegment", httpContent);
+                tracker += take;
+            }
+        }
+
+        [FunctionName("ProcessSegment")]
+        public static async Task ProcessSegments(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var copyMetadataQueue = GetCopyMetadataQueue();
+            var startedAt = DateTime.UtcNow;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (StreamReader streamReader = new StreamReader(req.Body))
+            {
+                var json = await streamReader.ReadToEndAsync();
+                var copyMetadataInfos = JsonConvert.DeserializeObject<List<CopyMetadataInfo>>(json);
+                var enqueueTasks = copyMetadataInfos
+                    .Select(copyMetadataInfo =>
+                        copyMetadataQueue.AddMessageAsync(new CloudQueueMessage(copyMetadataInfo.ToString())))
+                    .ToList();
+                await Task.WhenAll(enqueueTasks);
+                await LogProcessSegment(copyMetadataInfos.First().ExecutionId, startedAt, stopwatch.ElapsedMilliseconds, copyMetadataInfos.Count());
             }
         }
 
         [FunctionName("CopyMetadata")]
         public static async Task UpdateMetadata(
-            [QueueTrigger("copymetadata", Connection = "CopyBlobMetadataQueueConnectionString")]string blobName,
+            [QueueTrigger("copymetadata")]string json,
             ILogger log)
         {
-            var sourceBlobContainer = GetSourceBlobContainer();
-            var sourceBlob = sourceBlobContainer.GetBlockBlobReference(blobName);
+            var startedAt = DateTime.UtcNow;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var copyMetadataInfo = CopyMetadataInfo.Deserialize(json);
+
+            var sourceBlob =
+                GetBlobContainer(copyMetadataInfo.SourceConnectionString, copyMetadataInfo.SourceContainerName)
+                .GetBlockBlobReference(copyMetadataInfo.BlobName);
             await sourceBlob.FetchAttributesAsync();
 
-            var destinationBlobContainer = GetDestinationBlobContainer();
-            var destinationBlob = destinationBlobContainer.GetBlockBlobReference(blobName);
-            //await destinationBlob.FetchAttributesAsync(); // Do we need to fetch given we're overwriting?
+            if (!sourceBlob.Metadata.Any())
+            {
+                await LogCopyMetadata(copyMetadataInfo.ExecutionId, copyMetadataInfo.BlobName, startedAt, stopwatch.ElapsedMilliseconds, sourceBlob.Metadata.Count());
+                return;
+            }
+
+            var destinationBlob =
+                GetBlobContainer(copyMetadataInfo.DestinationConnectionString, copyMetadataInfo.DestinationContainerName)
+                .GetBlockBlobReference(copyMetadataInfo.BlobName);
+            //await destinationBlob.FetchAttributesAsync(); // No need to fetch as being replaced.
 
             destinationBlob.Metadata.Clear();
             foreach (var metadata in sourceBlob.Metadata)
@@ -71,33 +141,131 @@ namespace blobmetadataupdate
                 destinationBlob.Metadata.Add(metadata.Key, metadata.Value);
             }
             await destinationBlob.SetMetadataAsync();
+            await LogCopyMetadata(copyMetadataInfo.ExecutionId, copyMetadataInfo.BlobName, startedAt, stopwatch.ElapsedMilliseconds, sourceBlob.Metadata.Count());
         }
 
-        private static CloudBlobContainer GetSourceBlobContainer()
+        public class CopyMetadataInfo
         {
-            return GetBlobContainer(Environment.GetEnvironmentVariable("SourceConnectionString"), Environment.GetEnvironmentVariable("SourceContainerName"));
+            public string ExecutionId { get; set; }
+            public string BlobName { get; set; }
+            public Dictionary<string, string> Metadata { get; set; }
+            public string SourceConnectionString { get; set; }
+            public string SourceContainerName { get; set; }
+            public string DestinationConnectionString { get; set; }
+            public string DestinationContainerName { get; set; }
+
+            public static async Task<CopyMetadataInfo> Deserialize(Stream stream)
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    var json = await streamReader.ReadToEndAsync();
+                    return Deserialize(json);
+                }
+            }
+
+            public static CopyMetadataInfo Deserialize(string json)
+            {
+                return JsonConvert.DeserializeObject<CopyMetadataInfo>(json) ?? new CopyMetadataInfo();
+            }
+
+            public static string Serialize(CopyMetadataInfo copyMetadataInfo)
+            {
+                return JsonConvert.SerializeObject(copyMetadataInfo);
+            }
+
+            public override string ToString()
+            {
+                return Serialize(this);
+            }
         }
 
-        private static CloudBlobContainer GetDestinationBlobContainer()
+        private class CopyMetadataLog : TableEntity
         {
-            return GetBlobContainer(Environment.GetEnvironmentVariable("DestinationConnectionString"), Environment.GetEnvironmentVariable("DestinationContainerName"));
+            public string Function { get; set; }
+            public string BlobName { get; set; }
+            public DateTime StartedAt { get; set; }
+            public long Duration { get; set; }
+            public int Count { get; set; }
+
+            public CopyMetadataLog() { }
+        }
+
+        private static async Task LogListBlobs(string executionId, DateTime startedAt, long duration, int count)
+        {
+            await GetCopyMetadataTable()
+                .ExecuteAsync(TableOperation.Insert(new CopyMetadataLog()
+                {
+                    PartitionKey = executionId,
+                    RowKey = Guid.NewGuid().ToString(),
+                    Function = "ListBlobs",
+                    StartedAt = startedAt,
+                    Duration = duration,
+                    Count = count,
+                }));
+        }
+
+        private static async Task LogProcessSegment(string executionId, DateTime startedAt, long duration, int count)
+        {
+            await GetCopyMetadataTable()
+                .ExecuteAsync(TableOperation.Insert(new CopyMetadataLog()
+                {
+                    PartitionKey = executionId,
+                    RowKey = Guid.NewGuid().ToString(),
+                    Function = "ProcessSegment",
+                    StartedAt = startedAt,
+                    Duration = duration,
+                    Count = count,
+                }));
+        }
+
+        private static async Task LogCopyMetadata(string executionId, string blobName, DateTime startedAt, long duration, int count)
+        {
+            await GetCopyMetadataTable()
+                .ExecuteAsync(TableOperation.Insert(new CopyMetadataLog()
+                {
+                    PartitionKey = executionId,
+                    RowKey = Guid.NewGuid().ToString(),
+                    Function = "CopyMetadata",
+                    BlobName = blobName,
+                    StartedAt = startedAt,
+                    Duration = duration,
+                    Count = count,
+                }));
         }
 
         private static CloudBlobContainer GetBlobContainer(string connectionString, string containerName)
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference(containerName);
-            return cloudBlobContainer;
+            return
+                CloudStorageAccount.Parse(connectionString)
+                .CreateCloudBlobClient()
+                .GetContainerReference(containerName);
         }
 
-        private static CloudQueue GetQueue(string queueName)
+        private static CloudQueue GetCopyMetadataQueue()
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("CopyBlobMetadataQueueConnectionString"));
-            CloudQueueClient client = storageAccount.CreateCloudQueueClient();
-            CloudQueue queue = client.GetQueueReference(queueName);
-            queue.CreateIfNotExistsAsync();
-            return queue;
+            return
+                CloudStorageAccount.Parse(functionStorageConnectionString)
+                .CreateCloudQueueClient()
+                .GetQueueReference("copymetadata");
+        }
+
+        private static CloudTable GetCopyMetadataTable()
+        {
+            return
+                CloudStorageAccount.Parse(functionStorageConnectionString)
+                .CreateCloudTableClient()
+                .GetTableReference("CopyMetadata");
+        }
+
+        private static async Task EnsureInfrastructure()
+        {
+            var copyMetadataTable = GetCopyMetadataTable();
+            if (!await copyMetadataTable.ExistsAsync())
+                await copyMetadataTable.CreateAsync();
+
+            var copyMetadataQueue = GetCopyMetadataQueue();
+            if (!await copyMetadataQueue.ExistsAsync())
+                await copyMetadataQueue.CreateAsync();
         }
     }
 }
